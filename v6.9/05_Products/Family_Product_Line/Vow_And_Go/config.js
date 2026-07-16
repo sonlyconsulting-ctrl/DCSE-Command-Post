be5f4dfe7fbf4e38939238956bd83daa0fb50074
sonlyconsulting-ctrl <sonlyconsulting@gmail.com>
2026-07-16 14:53:01 -0400

    Add Vow & Go Supabase client configuration

---
+++ v6.9/05_Products/Family_Product_Line/Vow_And_Go/config.js
@@ -0,0 +1,7 @@
+window.VOW_GO_CONFIG = Object.freeze({
+  supabaseUrl: "https://ajwqmgwjtxonhkvngoca.supabase.co",
+  supabasePublishableKey: "sb_publishable_rzkLTZp4QDThlP7AgGoFPQ_e9OR9PzA",
+  productSlug: "akira-connor-vow-go",
+  supportEmail: "sonlyconsulting@gmail.com",
+  defaultWeddingDate: "2027-06-12T16:00:00-10:00"
+});
